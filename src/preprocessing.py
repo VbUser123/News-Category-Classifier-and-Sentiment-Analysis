eee8cd92154fbd5e43c2533158dc82369475fb99
VbUser123 <178082008+VbUser123@users.noreply.github.com>
2026-06-14 19:14:17 +0530

    Fix NLTK punkt_tab deployment issue

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,6 +2,7 @@ import string
 import nltk
 
 nltk.download("punkt", quiet=True)
+nltk.download("punkt_tab", quiet=True)
 nltk.download("stopwords", quiet=True)
 nltk.download("wordnet", quiet=True)
 
